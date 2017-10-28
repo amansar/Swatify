946ac47ca46f0ef54cac21b6e7f49fc91d2c0d94
Oliver Newman <oliver@whmsi.com>
2017-10-28 16:49:21 -0400

    Add HibernateUtil.saveObject helper

--- src/main/java/edu/swarthmore/cs/cs71/swatify/util/HibernateUtil.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/util/HibernateUtil.java
@@ -1,14 +1,23 @@
 package edu.swarthmore.cs.cs71.swatify.util;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-// Source: http://fruzenshtein.com/hibernate-mysql-maven-tutorial/
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+    /**
+     * Save an object to the database.
+     * @param object The object to persist.
+     */
+    public static void saveObject(Object object) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(object);
+        session.getTransaction().commit();
+
+        session.close();
     }
 }
