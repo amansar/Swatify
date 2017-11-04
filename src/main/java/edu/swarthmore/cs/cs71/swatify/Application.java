d6d883794f599db4546c5e491e79a5b22e684232
Oliver Newman <oliver@whmsi.com>
2017-11-04 17:16:51 -0400

    Remove debugginf stuff from Application.java

--- src/main/java/edu/swarthmore/cs/cs71/swatify/Application.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/Application.java
@@ -2,19 +2,12 @@ package edu.swarthmore.cs.cs71.swatify;
 
 import edu.swarthmore.cs.cs71.swatify.controllers.ArtistsController;
 import edu.swarthmore.cs.cs71.swatify.controllers.UsersController;
-import edu.swarthmore.cs.cs71.swatify.models.User;
-import edu.swarthmore.cs.cs71.swatify.util.HibernateUtil;
 
 import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
         setRoutes();
-
-        User user = new User();
-        user.setEmail("oliver@whmsi.com");
-        user.setUsername("oliver@whmsi.com");
-        HibernateUtil.saveObject(user);
     }
 
     private static void setRoutes() {
