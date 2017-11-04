68005afe61d5bad92fcda83839038efa5f404e56
Oliver Newman <oliver@whmsi.com>
2017-11-04 13:41:50 -0400

    Refactor model-specific routes into controllers

--- src/main/java/edu/swarthmore/cs/cs71/swatify/Application.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/Application.java
@@ -1,22 +1,26 @@
 package edu.swarthmore.cs.cs71.swatify;
 
+import edu.swarthmore.cs.cs71.swatify.controllers.ArtistsController;
 import edu.swarthmore.cs.cs71.swatify.controllers.UsersController;
+import edu.swarthmore.cs.cs71.swatify.models.User;
+import edu.swarthmore.cs.cs71.swatify.util.HibernateUtil;
 
 import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
         setRoutes();
+
+        User user = new User();
+        user.setEmail("oliver@whmsi.com");
+        user.setUsername("oliver@whmsi.com");
+        HibernateUtil.saveObject(user);
     }
 
     private static void setRoutes() {
         path("/api/v1", () -> {
-            path("/users", () -> {
-                get("/*", UsersController::getUser);
-                put("/*", UsersController::createUser);
-                patch("/*", UsersController::updateUser);
-                delete("/*", UsersController::deleteUser);
-            });
+            new UsersController();
+            new ArtistsController();
         });
     }
 }
--- src/main/java/edu/swarthmore/cs/cs71/swatify/controllers/UsersController.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/controllers/UsersController.java
@@ -1,27 +1,40 @@
 package edu.swarthmore.cs.cs71.swatify.controllers;
 
 import edu.swarthmore.cs.cs71.swatify.models.User;
+import edu.swarthmore.cs.cs71.swatify.util.GsonUtil;
 import edu.swarthmore.cs.cs71.swatify.util.HibernateUtil;
-import spark.Request;
-import spark.Response;
+
+import static spark.Spark.*;
 
 public class UsersController {
-    public static User getUser(Request request, Response response) {
-        int userId = Integer.parseInt(request.params("id"));
-        return HibernateUtil.getObjectById(User.class, userId);
-    }
+    public UsersController() {
+        path("/users", () -> {
+            get("/:id", (request, response) -> {
+                int id = Integer.parseInt(request.params("id"));
+                return HibernateUtil.getObjectById(User.class, id);
+            }, GsonUtil::toJson);
 
-    public static User createUser(Request request, Response response) {
-        return new User("","");
-    }
+            post("", (request, response) -> {
+                User user = GsonUtil.fromJson(User.class, request.body());
+                return HibernateUtil.saveObject(user);
+            }, GsonUtil::toJson);
 
-    public static User updateUser(Request request, Response response) {
-        int id = Integer.parseInt(request.params("id"));
-        return new User("","");
-    }
+            patch("/:id", (request, response) -> {
+                User user = GsonUtil.fromJson(User.class, request.body());
+                return HibernateUtil.updateObject(user);
+            }, GsonUtil::toJson);
 
-    public static User deleteUser(Request request, Response response) {
-        int id = Integer.parseInt(request.params("id"));
-        return new User("","");
+            delete("/:id", (request, response) -> {
+                int userId = Integer.parseInt(request.params("id"));
+                return HibernateUtil.deleteObject(User.class, userId);
+            }, GsonUtil::toJson);
+
+            after((req, res) -> res.type("application/json"));
+
+            exception(IllegalArgumentException.class, (e, req, res) -> {
+                res.status(400);
+            });
+
+        });
     }
 }
