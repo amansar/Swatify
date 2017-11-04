206530e01c2cfbf628b7a5a912f47991f145370a
Oliver Newman <oliver@whmsi.com>
2017-11-04 13:42:11 -0400

    Clean up User class

--- src/main/java/edu/swarthmore/cs/cs71/swatify/models/User.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/models/User.java
@@ -16,16 +16,8 @@ public class User {
 
     private String username;
     private String email;
-    private List<User> following;
-    private List<User> followers;
-    private List<PublicAction> feed;
 
     public User() { }
-
-    public User(String username, String email) {
-        this.username = username;
-        this.email = email;
-    }
 
     public int getId() {
         return id;
@@ -35,35 +27,15 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getEmail() {
         return email;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public List<User> getFollowing() { return following;}
-
-    public List<User> getFollowers() { return followers;}
-
-    public void followUser(User user){
-        following.add(user);
-        HibernateUtil.updateObject(following); //need to update user object instead?
-    }
-
-    public void unfollowUser(User user){
-        if (following.contains(user)) {
-            following.remove(user);
-        }
-        HibernateUtil.updateObject(following); //need to update user object instead?
-    }
-
-    public List<PublicAction> getActions() {
-        return feed;
     }
 }
