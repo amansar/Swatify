a7287627aabf41e27e06c687c92e919e1e9ffea7
Oliver Newman <oliver@whmsi.com>
2017-11-04 13:42:43 -0400

    Add deleteObject method to HibernateUtil

--- src/test/java/edu/swarthmore/cs/cs71/swatify/util/HibernateUtilTest.java
+++ src/test/java/edu/swarthmore/cs/cs71/swatify/util/HibernateUtilTest.java
@@ -2,6 +2,5 @@ package edu.swarthmore.cs.cs71.swatify.util;
 
 import org.junit.*;
 
-import static org.junit.Assert.assertThat;
-
-public class HibernateUtilTest { }
+public class HibernateUtilTest {
+}
