4514ab45b0eab2a92f567117f0148e425ada0607
Oliver Newman <oliver@whmsi.com>
2017-11-05 19:49:38 -0500

    Refactor UsersController to have many methods

--- src/main/java/edu/swarthmore/cs/cs71/swatify/controllers/UsersController.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/controllers/UsersController.java
@@ -3,31 +3,18 @@ package edu.swarthmore.cs.cs71.swatify.controllers;
 import edu.swarthmore.cs.cs71.swatify.models.User;
 import edu.swarthmore.cs.cs71.swatify.util.GsonUtil;
 import edu.swarthmore.cs.cs71.swatify.util.HibernateUtil;
+import spark.Request;
+import spark.Response;
 
 import static spark.Spark.*;
 
 public class UsersController {
     public UsersController() {
         path("/users", () -> {
-            get("/:id", (request, response) -> {
-                int id = Integer.parseInt(request.params("id"));
-                return HibernateUtil.getObjectById(User.class, id);
-            }, GsonUtil::toJson);
-
-            post("", (request, response) -> {
-                User user = GsonUtil.fromJson(User.class, request.body());
-                return HibernateUtil.saveObject(user);
-            }, GsonUtil::toJson);
-
-            patch("/:id", (request, response) -> {
-                User user = GsonUtil.fromJson(User.class, request.body());
-                return HibernateUtil.updateObject(user);
-            }, GsonUtil::toJson);
-
-            delete("/:id", (request, response) -> {
-                int userId = Integer.parseInt(request.params("id"));
-                return HibernateUtil.deleteObject(User.class, userId);
-            }, GsonUtil::toJson);
+            get("/:id", this::getUser, GsonUtil::toJson);
+            post("", this::createUser, GsonUtil::toJson);
+            patch("/:id", this::updateUser, GsonUtil::toJson);
+            delete("/:id", this::deleteUser, GsonUtil::toJson);
 
             after((req, res) -> res.type("application/json"));
 
@@ -37,4 +24,25 @@ public class UsersController {
 
         });
     }
+
+    User getUser(Request request, Response response) {
+        int id = Integer.parseInt(request.params("id"));
+        return HibernateUtil.getObjectById(User.class, id);
+    }
+
+    User createUser(Request request, Response response) {
+        User user = GsonUtil.fromJson(User.class, request.body());
+        return HibernateUtil.saveObject(user);
+    }
+
+    User updateUser(Request request, Response response) {
+        User user = GsonUtil.fromJson(User.class, request.body());
+        return HibernateUtil.updateObject(user);
+    }
+
+    boolean deleteUser(Request request, Response response) {
+        int userId = Integer.parseInt(request.params("id"));
+        return HibernateUtil.deleteObject(User.class, userId);
+    }
+
 }
