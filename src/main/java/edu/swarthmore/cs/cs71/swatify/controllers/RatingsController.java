59e84911cb68da8c3326a099117784537f193b9f
Colin Pillsbury <cpillsb1@swarthmore.edu>
2017-11-05 21:55:45 -0500

    working on public action controllers

--- src/main/java/edu/swarthmore/cs/cs71/swatify/controllers/RatingsController.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/controllers/RatingsController.java
@@ -1,4 +1,43 @@
 package edu.swarthmore.cs.cs71.swatify.controllers;
 
+import edu.swarthmore.cs.cs71.swatify.models.Rating;
+import edu.swarthmore.cs.cs71.swatify.util.GsonUtil;
+import edu.swarthmore.cs.cs71.swatify.util.HibernateUtil;
+
+import static spark.Spark.*;
+
 public class RatingsController {
+    public RatingsController() {
+        path("/Ratings", () -> {
+            get("/:id", (request, response) -> getRating(Integer.parseInt(request.params("id"))), GsonUtil::toJson);
+
+            post("", (request, response) -> createRating(GsonUtil.fromJson(Rating.class, request.body())),  GsonUtil::toJson);
+
+            patch("/:id", (request, response) -> updateRating(GsonUtil.fromJson(Rating.class, request.body())), GsonUtil::toJson);
+
+            delete("/:id", (request, response) -> deleteRating(Integer.parseInt(request.params("id"))), GsonUtil::toJson);
+
+            after((req, res) -> res.type("application/json"));
+
+            exception(IllegalArgumentException.class, (e, req, res) -> {
+                res.status(400);
+            });
+        });
+    }
+
+    public static Rating getRating(int id) {
+        return HibernateUtil.getObjectById(Rating.class, id);
+    }
+
+    public static Rating createRating(Rating Rating) {
+        return HibernateUtil.saveObject(Rating);
+    }
+
+    public static Rating updateRating(Rating Rating) {
+        return HibernateUtil.updateObject(Rating);
+    }
+
+    public static Rating deleteRating(int id) {
+        return HibernateUtil.deleteObject(Rating.class, id);
+    }
 }
