0868b2a13b5dd29611b522aa24b28379f0c2d0de
Cameron Marsh <cmarsh10@gmail.com>
2017-11-05 21:38:45 -0500

    added artist controller and tests

--- src/test/java/edu/swarthmore/cs/cs71/swatify/controllers/ArtistsControllerTest.java
+++ src/test/java/edu/swarthmore/cs/cs71/swatify/controllers/ArtistsControllerTest.java
@@ -1,7 +1,38 @@
 package edu.swarthmore.cs.cs71.swatify.controllers;
 
 import edu.swarthmore.cs.cs71.swatify.models.Artist;
+import edu.swarthmore.cs.cs71.swatify.util.HibernateUtil;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ArtistsControllerTest {
 
+    @Test
+    public void shouldCreateNewArtist() throws Exception {
+        Artist cameron = new Artist();
+        cameron.setName("Cameron");
+        cameron.setSpotifyId(null);
+
+        Artist createdArtist = ArtistsController.createArtist(cameron);
+
+        Assert.assertEquals(cameron.getName(), createdArtist.getName());
+        Assert.assertEquals(cameron.getSpotifyId(), createdArtist.getSpotifyId());
+    }
+
+    @Test
+    public void shouldGetExistingArtist() throws Exception {
+        Artist zach = new Artist();
+        zach.setName("Zachariah");
+        zach.setSpotifyId("ljh46kjh436jbnk6j2bk2624");
+
+        HibernateUtil.saveObject(zach);
+
+        int id = zach.getId();
+
+        Artist existingArtist = HibernateUtil.getObjectById(Artist.class, id);
+
+        Assert.assertEquals(zach.getName(), existingArtist.getName());
+        Assert.assertEquals(zach.getSpotifyId(), existingArtist.getSpotifyId());
+        Assert.assertEquals(id, existingArtist.getId());
+    }
 }
