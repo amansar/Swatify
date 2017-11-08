b40388ca1a7f620564b7abb34a1820a2ca89ab47
Elsher Abraham <eabraha1@swarthmore.edu>
2017-11-08 13:34:20 -0500

    User.java

---
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/models/User.java
@@ -0,0 +1,104 @@
+package edu.swarthmore.cs.cs71.swatify.models;
+
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table
+@DynamicUpdate
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    @NotBlank
+    private String username;
+
+    @Column(unique = true)
+    @NotBlank
+    private String email;
+<<<<<<< HEAD
+    private ArrayList<User> following;  // figure out follow implementation
+    private ArrayList<User> followers;
+    private ArrayList<PublicAction> feed;
+    public User(String username, String email) {
+=======
+
+    @Column(unique = true)
+    @NotBlank
+    private String spotifyId;
+
+    public User() { }
+
+    public User(String username, String email, String spotifyId) {
+>>>>>>> 827907112d2d7d7930a63ef76acc8f50a1f1d9bc
+        this.username = username;
+        this.email = email;
+        this.spotifyId = spotifyId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+<<<<<<< HEAD
+    public void setUsername(String username) {
+        this.username = username;
+        HibernateUtil.updateObject(this);
+    }
+
+=======
+>>>>>>> 827907112d2d7d7930a63ef76acc8f50a1f1d9bc
+    public String getEmail() {
+        return email;
+    }
+
+<<<<<<< HEAD
+    public void setEmail(String email) {
+        this.email = email;
+        HibernateUtil.updateObject(this);
+    }
+
+    public ArrayList<User> getFollowing() { return following;}
+
+    public ArrayList<User> getFollowers() { return followers;}
+
+    public void followUser(User user){
+        following.add(user);
+        HibernateUtil.updateObject(following); //need to update user object instead?
+        HibernateUtil.updateObject(this);
+    }
+
+    public void unfollowUser(User user){
+        if (following.contains(user)) {
+            following.remove(user);
+        }
+        HibernateUtil.updateObject(following); //need to update user object instead?
+        HibernateUtil.updateObject(this);
+=======
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpotifyId() {
+        return spotifyId;
+>>>>>>> 827907112d2d7d7930a63ef76acc8f50a1f1d9bc
+    }
+
+    public void setSpotifyId(String spotifyId) {
+        this.spotifyId = spotifyId;
+    }
+}
