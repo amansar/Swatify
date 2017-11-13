2acd0b29dfa797829279bdadf6e6de7b2c3b0b64
Oliver Newman <oliver@whmsi.com>
2017-11-13 13:11:15 -0500

    Add frontend setup info to README

--- src/main/java/edu/swarthmore/cs/cs71/swatify/Application.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/Application.java
@@ -1,29 +1,16 @@
 package edu.swarthmore.cs.cs71.swatify;
 
-import com.github.javafaker.Faker;
 import edu.swarthmore.cs.cs71.swatify.controllers.AlbumsController;
 import edu.swarthmore.cs.cs71.swatify.controllers.ArtistsController;
 import edu.swarthmore.cs.cs71.swatify.controllers.UsersController;
-import edu.swarthmore.cs.cs71.swatify.models.User;
 
 import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
-//        setRoutes();
+        staticFiles.location("/frontend");
 
-        staticFiles.location("/public");
-
-        User user = new User("oliver", "onewman1@swarthmore.edu", "asdfsadf");
-        UsersController.createUser(user);
-
-        Faker faker;
-        for (int i = 0; i < 50; i++) {
-            faker = new Faker();
-            user = new User(faker.name().username(), faker.internet().emailAddress(), faker.crypto().sha256());
-            UsersController.createUser(user);
-            System.out.println(user.getId());
-        }
+        setRoutes();
     }
 
     public static void setRoutes() {
