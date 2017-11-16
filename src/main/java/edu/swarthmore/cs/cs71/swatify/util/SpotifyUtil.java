0cce1e2100b6be0ab47e86c0c362c274697ac4e8
Oliver Newman <oliver@whmsi.com>
2017-11-16 10:17:20 -0500

    Uncomment SpotifyUtil

--- src/main/java/edu/swarthmore/cs/cs71/swatify/util/SpotifyUtil.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/util/SpotifyUtil.java
@@ -15,80 +15,78 @@ import com.wrapper.spotify.models.SimpleAlbum;
 /*Will need to login application and get Client_ID and Client Secrets to use this application */
 public class SpotifyUtil {
 
-//    private Api spotifyApi;
-//
-//    public SpotifyUtil () {
-//
-//        final String clientId = Secrets.SPOTIFY_CLIENT_ID;
-//        final String clientSecret = Secrets.SPOTIFY_CLIENT_SECRET;
-//
-//         spotifyApi = Api.builder()
-//                .clientId(clientId)
-//                .clientSecret(clientSecret)
-//                .build();
-//
-//        /* Create a request object. */
-//        final ClientCredentialsGrantRequest request = spotifyApi.clientCredentialsGrant().build();
-//
-//        /* Use the request object to make the request, either asynchronously (getAsync) or synchronously (get) */
-//        final SettableFuture<ClientCredentials> responseFuture = request.getAsync();
-//
-//        /* Add callbacks to handle success and failure */
-//        Futures.addCallback(responseFuture, new FutureCallback<ClientCredentials>() {
-//            @Override
-//            public void onSuccess(ClientCredentials clientCredentials) {
-//                /* The tokens were retrieved successfully! */
-//                System.out.println("Successfully retrieved an access token! " + clientCredentials.getAccessToken());
-//                System.out.println("The access token expires in " + clientCredentials.getExpiresIn() + " seconds");
-//
-//                /* Set access token on the Api object so that it's used going forward */
-//                spotifyApi.setAccessToken(clientCredentials.getAccessToken());
-//
-//                /* Please note that this flow does not return a refresh token.
-//                * That's only for the Authorization code flow */
-//            }
-//
-//            @Override
-//            public void onFailure(Throwable throwable) {
-//                System.out.println( "An error occurred while getting the access token. This is probably caused by the client id or client secret is invalid.  ");
-//            }
-//        });
-//
-//    }
-//
-//    private static Api getSpotifyAPI() {
-//        final String clientId = Secrets.SPOTIFY_CLIENT_ID;
-//        final String clientSecret = Secrets.SPOTIFY_CLIENT_SECRET;
-//        //final String redirectURI = "http://127.0.0.1:4567/api/token"; Don't need redirectURI unless we're doing User authentication
-//        return Api.builder()
-//                .clientId(clientId)
-//                .clientSecret(clientSecret)
-//                .build();
-//    }
-//
-//
-//
-//    public boolean searchAlbum (String query) {
-//        final AlbumSearchRequest request = this.spotifyApi.searchAlbums(query).offset(0).limit(3).build();
-//
-//        try {
-//            final Page<SimpleAlbum> albumSearchResult = request.get();
-//
-//            System.out.println("Printing results..");
-//            for (SimpleAlbum album : albumSearchResult.getItems()) {
-//                if(album.getName().toLowerCase().contains(query.toLowerCase())){
-//                    return true;
-//                }
-//            }
-//
-//        } catch (Exception e) {
-//            System.out.println("Something went wrong!" + e.getMessage());
-//        }
-//
-//        return false;
-//    }
+    private Api spotifyApi;
+
+    public SpotifyUtil () {
+
+        final String clientId = Secrets.SPOTIFY_CLIENT_ID;
+        final String clientSecret = Secrets.SPOTIFY_CLIENT_SECRET;
+
+         spotifyApi = Api.builder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+
+        /* Create a request object. */
+        final ClientCredentialsGrantRequest request = spotifyApi.clientCredentialsGrant().build();
+
+        /* Use the request object to make the request, either asynchronously (getAsync) or synchronously (get) */
+        final SettableFuture<ClientCredentials> responseFuture = request.getAsync();
+
+        /* Add callbacks to handle success and failure */
+        Futures.addCallback(responseFuture, new FutureCallback<ClientCredentials>() {
+            @Override
+            public void onSuccess(ClientCredentials clientCredentials) {
+                /* The tokens were retrieved successfully! */
+                System.out.println("Successfully retrieved an access token! " + clientCredentials.getAccessToken());
+                System.out.println("The access token expires in " + clientCredentials.getExpiresIn() + " seconds");
+
+                /* Set access token on the Api object so that it's used going forward */
+                spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+                /* Please note that this flow does not return a refresh token.
+                * That's only for the Authorization code flow */
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println( "An error occurred while getting the access token. This is probably caused by the client id or client secret is invalid.  ");
+            }
+        });
+
+    }
+
+    private static Api getSpotifyAPI() {
+        final String clientId = Secrets.SPOTIFY_CLIENT_ID;
+        final String clientSecret = Secrets.SPOTIFY_CLIENT_SECRET;
+        //final String redirectURI = "http://127.0.0.1:4567/api/token"; Don't need redirectURI unless we're doing User authentication
+        return Api.builder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+    }
 
 
+
+    public boolean searchAlbum (String query) {
+        final AlbumSearchRequest request = this.spotifyApi.searchAlbums(query).offset(0).limit(3).build();
+
+        try {
+            final Page<SimpleAlbum> albumSearchResult = request.get();
+
+            System.out.println("Printing results..");
+            for (SimpleAlbum album : albumSearchResult.getItems()) {
+                if(album.getName().toLowerCase().contains(query.toLowerCase())){
+                    return true;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong!" + e.getMessage());
+        }
+
+        return false;
+    }
 }
 
 
