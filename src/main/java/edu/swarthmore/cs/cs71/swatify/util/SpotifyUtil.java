62eef4fc186963d5669f1780d066a39c84417d98
Cameron Marsh <cmarsh10@gmail.com>
2017-11-19 20:32:19 -0500

    fixed conflict wiith SpotifyUtil

--- src/main/java/edu/swarthmore/cs/cs71/swatify/util/SpotifyUtil.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/util/SpotifyUtil.java
@@ -64,15 +64,11 @@ public class SpotifyUtil {
 
     }
 
-<<<<<<< HEAD
+
     public static Api getSpotifyAPI() {
         final String clientId = Secrets.SPOTIFY_CLIENT_ID;
         final String clientSecret = Secrets.SPOTIFY_CLIENT_SECRET;
-=======
-    private static Api getSpotifyAPI() {
-        final String clientId = templatedSecrets.SPOTIFY_CLIENT_ID;
-        final String clientSecret = templatedSecrets.SPOTIFY_CLIENT_SECRET;
->>>>>>> e81d6a36bb4bc0de96daa9d7fe59df485cbeb229
+
         //final String redirectURI = "http://127.0.0.1:4567/api/token"; Don't need redirectURI unless we're doing User authentication
         return Api.builder()
                 .clientId(clientId)
@@ -102,39 +98,7 @@ public class SpotifyUtil {
         return false;
     }
 
-
-<<<<<<< HEAD
-=======
-    public  Artist getArtistInfo(String artistId){
-        final ArtistRequest request = this.spotifyApi.getArtist(artistId).build();
-
-        try{
-            final Artist requestedArtist = request.get();
-
-            return requestedArtist;
-
-        } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Something went wrong...");
-        }
-
-        return null;
-    }
-
-    public  List<Album> getArtistAlbums (String artistId) {
-        final AlbumsForArtistRequest albumsRequest = this.spotifyApi.getAlbumsForArtist(artistId).build();
-
-        try{
-            final List<Album> albums = (List<Album>) albumsRequest.get();
-            return albums;
-        } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Something went wrong...");
-        }
-
-        return null;
-    }
->>>>>>> e81d6a36bb4bc0de96daa9d7fe59df485cbeb229
+   
 }
 
 
