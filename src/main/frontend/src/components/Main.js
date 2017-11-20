43a35e8f323e6de449bf676a06a59deda921d767
Cameron Marsh <cmarsh10@gmail.com>
2017-11-19 19:16:58 -0500

    added Main and Feed to frontend again

---
+++ src/main/frontend/src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Feed from './Feed';
+import Discover from './Discover';
+import Discuss from './Discuss';
+import Connect from './Connect';
+import Artist from './Artist';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact from='/' to='/feed' />
+        <Route path='/feed' component={Feed} />
+        <Route path='/discover' component={Discover} />
+        <Route path='/discuss' component={Discuss} />
+        <Route path='/connect' component={Connect} />
+        <Route path='/artist' component={Artist} />
+      </Switch>
+    );
+  }
+}
--- src/test/java/edu/swarthmore/cs/cs71/swatify/util/SpotifyUtilTest.java
+++ src/test/java/edu/swarthmore/cs/cs71/swatify/util/SpotifyUtilTest.java
@@ -1,5 +1,6 @@
 package edu.swarthmore.cs.cs71.swatify.util;
 
+import com.wrapper.spotify.models.Artist;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,5 +9,11 @@ public class SpotifyUtilTest {
     @Test
     public void shouldPrintAlbumResults(){
         SpotifyUtil spotifyUtil = new SpotifyUtil();
+    }
+
+    @Test
+    public void shouldGetArtistInfo() throws Exception {
+        edu.swarthmore.cs.cs71.swatify.models.Artist knxwledge = new edu.swarthmore.cs.cs71.swatify.models.Artist("Knxwledge", "17Zu03OgBVxgLxWmRUyNOJ");
+        Artist spotifyArtist = SpotifyUtil.getArtistInfo(knxwledge.getSpotifyId());
     }
 }
