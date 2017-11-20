c21a32faa7a547662df4a977a1e92c31e92fb86e
Colin Pillsbury <cpillsb1@swarthmore.edu>
2017-11-20 02:16:34 -0500

    set up album page to work with spotify

--- src/main/frontend/src/components/Album.js
+++ src/main/frontend/src/components/Album.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 
 
 class Album extends Component {
-    state = {loading: true, album: null}
+    state = {loading: true}
 
     componentDidMount(){
-        fetch('/api/v1/albums/' + this.props.match.params.id)
+        fetch('/api/v1/albums/7gsWAHLeT0w7es6FofOXk1')
               .then(res => res.json())
-              .then(album => this.setState({ album }));
+              .then(album => this.setState({ album: album.name,
+                                artist: album.artists[0].name,
+                                image: album.images[0].url} ));
     }
 
     render() {
-        return (<h1>this.state.album.getName</h1>);
+        return (
+            <div>
+                <h1>{this.state.album}</h1>
+                <h2>{this.state.artist}</h2>
+                <img src={this.state.image} alt="" height="200" width="200"></img>
+            </div>);
        // <h1>this.state.album.getName</h1>
     }
 }
