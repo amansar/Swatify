83b671a572ad2af728ce7169e268d71df721dd23
Cameron Marsh <cmarsh1@cs.swarthmore.edu>
2017-11-22 13:54:21 -0500

    started adding search tests:

---
+++ src/test/java/edu/swarthmore/cs/cs71/swatify/controllers/SearchControllerTest.java
@@ -0,0 +1,21 @@
+package edu.swarthmore.cs.cs71.swatify.controllers;
+
+import org.junit.Test;
+
+public class SearchControllerTest {
+
+    @Test
+    public void shouldReturnTrackSearchResults() throws Exception {
+
+    }
+
+    @Test
+    public void shouldReturnAlbumSearchResults() throws Exception {
+
+    }
+
+    @Test
+    public void shouldReturnArtistSearchResults() throws Exception {
+        
+    }
+}
