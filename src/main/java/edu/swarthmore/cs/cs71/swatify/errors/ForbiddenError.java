9fa9f40d6dbe8272c08b177f7727d5b50ac439f0
Oliver Newman <oliver@whmsi.com>
2017-11-29 14:52:32 -0500

    Start refactoring controllers and adding error objects

--- src/main/java/edu/swarthmore/cs/cs71/swatify/errors/ForbiddenError.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/errors/ForbiddenError.java
@@ -1,9 +1,9 @@
 package edu.swarthmore.cs.cs71.swatify.errors;
 
-public class NotFoundError extends Error {
+public class ForbiddenError extends Error {
     private String message;
 
-    public NotFoundError(String message) {
-        super(404, message);
+    public ForbiddenError(String message) {
+        super(403, message);
     }
 }
--- src/main/java/edu/swarthmore/cs/cs71/swatify/errors/NotFoundError.java
+++ src/main/java/edu/swarthmore/cs/cs71/swatify/errors/NotFoundError.java
@@ -1,4 +1,9 @@
 package edu.swarthmore.cs.cs71.swatify.errors;
 
-public class NotFoundError {
+public class NotFoundError extends Error {
+    private String message;
+
+    public NotFoundError(String message) {
+        super(404, message);
+    }
 }
