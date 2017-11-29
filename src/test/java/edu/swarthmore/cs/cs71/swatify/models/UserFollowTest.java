696a0ac561e29d5e49195f47b7aac0532913c155
Elsher <eabraha1@swarthmore.edu>
2017-11-29 14:20:50 -0500

    test works

--- src/test/java/edu/swarthmore/cs/cs71/swatify/models/UserFollowTest.java
+++ src/test/java/edu/swarthmore/cs/cs71/swatify/models/UserFollowTest.java
@@ -4,12 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserFollowTest {
-/*
+
   public static void main(String[] args){
       UserFollowTest test = new UserFollowTest();
       test.shouldCreateUserFollow();
   }
-  */
+  
     @Test
   public void shouldCreateUserFollow() {
     User elsher = new User();
