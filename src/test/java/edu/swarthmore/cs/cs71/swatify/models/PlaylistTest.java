8e0c6bec5ce5bced6b66dcc31467889a21df326b
Elsher <eabraha1@swarthmore.edu>
2017-12-02 18:50:39 -0500

    added PlaylistTest.java

---
+++ src/test/java/edu/swarthmore/cs/cs71/swatify/models/PlaylistTest.java
@@ -0,0 +1,45 @@
+package edu.swarthmore.cs.cs71.swatify.models;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PlaylistTest {
+  @Test
+  public void testPlaylistFieldsAndMethods(){
+    Playlist playlist = new Playlist();
+
+    Track song1 = new Track();
+    Track song2 = new Track();
+    song1.setLengthOfTrack(1.11);
+    song1.setSpotifyID("abcdefghijk");
+    song1.setTitle("Song One");
+    song2.setLengthOfTrack(2.22);
+    song2.setSpotifyID("lmnopqrstuv");
+    song2.setTitle("Song Two");
+
+    playlist.add(song1);
+    assertEquals(true, playlist.contains(song1));
+    assertEquals(1.11, playlist.getLength());
+    playlist.add(song2);
+    assertEquals(2, playlist.getNumberOfSongs());
+    assertEquals(3.33, playlist.getLength());
+
+    playlist.setTitle("Playlist One");
+    assertEquals("Playlist One", playlist.getTitle());
+
+    playlist.setSpotifyID("ooooooooooo");
+    assertEquals("ooooooooooo", playlist.getSpotifyID());
+
+    playlist.setID(0);
+    assertEquals(0, playlist.getID());
+
+    playlist.setPrivacy(0);
+    assertEquals(true, playlist.isPublic());
+
+    assertEquals(song1, playlist.getTracklist()[0]);
+    
+    playlist.remove(song1);
+    assertEquals(1, playlist.getNumberOfSongs());
+    assertEquals(2.22, playlist.getLength());
+  }
+}
